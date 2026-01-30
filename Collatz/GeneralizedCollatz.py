1bd3d5e0722351214530f401860feb1505418d99
Anthony Kozar <akozar@users.sourceforge.net>
2026-01-30 00:11:05 -0500

    Allow testing multiple functions in a single run and print detailed reports about cycles, etc.

--- Collatz/GeneralizedCollatz.py
+++ Collatz/GeneralizedCollatz.py
@@ -15,11 +15,28 @@ def f5xp1(n):
     else:
         return 5*n+1
 
+def f5xp1d6(n):
+    if (n%2) == 0:
+        return n/2
+    elif (n%3) == 0:
+        return n/3
+    else:
+        return 5*n+1
+
 def f3xp5(n):
     if (n%2) == 0:
         return n/2
     else:
         return 3*n+5
+
+# Returns a new function f(n) that equals n/d when n is divisible by d or an+b when it is not.
+def newcollatzfunc(a, b, d = 2):
+    def collfunc(n):
+        if (n%d) == 0:
+            return n/d
+        else:
+            return a*n+b
+    return collfunc
 
 # Print the sequence of numbers derived from iteratively applying
 # collfunc to n until it reaches 1 or exceeds maxnum.
@@ -126,3 +143,59 @@ def analyzegraph(graph):
                 classes[m] = pd
                 notcyclic.append(m)
     return (cycles, classes)
+
+def cyclereport(cycles, classes, printleadinglists = True,  printdivergents = True, maxlistlen = 100, maxnum = 10**12):
+    # workaround for bug in Pyonic
+    partOfCycle = 1
+    leadsToCycle = 2
+    possiblyDivergent = 3
+
+    numcycles = len(cycles)
+    # create lists of numbers that lead to each cycle or that might diverge
+    if printleadinglists or printdivergents:
+        leadgbycyc = [list() for i in xrange(numcycles)]
+        divergent = []
+        for n in classes:
+            if classes[n][0] == leadsToCycle and printleadinglists and n <= maxnum:
+                leadgbycyc[classes[n][1]].append(n)
+            elif classes[n][0] == possiblyDivergent and printdivergents and n <= maxnum:
+                divergent.append(n)
+    
+    # print each cycle beginning with its smallest value and print its list of leading numbers (if requested)
+    for i in xrange(numcycles):
+        cycmin = min(cycles[i])
+        minidx = cycles[i].index(cycmin)
+        cyc = cycles[i][minidx:] + cycles[i][0:minidx]
+        print "Cycle %d (len %d):\n" % (i, len(cyc))
+        print " ", " ".join(map(str, cyc))
+        print
+        if printleadinglists:
+            leadgbycyc[i].sort()
+            print "  Numbers leading to cycle %d (%d):" % (i, len(leadgbycyc[i]))
+            print "   ", " ".join(map(str, leadgbycyc[i][:maxlistlen]))
+            print
+    
+    if printdivergents:
+        divergent.sort()
+        print "Numbers that appear to diverge (%d):\n" % (len(divergent))
+        print " ".join(map(str, divergent[:maxlistlen]))
+        print
+
+# Analyze the iterated behavior of collfunc for inputs 1 to maxtest and print a 
+# detailed report of the cycles found, which numbers lead to each cycle, and which 
+# numbers appear to diverge.
+def dolongreport(collfunc, maxtest, listlen = 100, maxint = 10**12):
+    g = makegraph(1, maxtest, 1, collfunc, False, maxint)
+    cycles,clss = analyzegraph(g)
+    cyclereport(cycles, clss, printleadinglists = True,  printdivergents = True, maxlistlen = listlen, maxnum = maxtest)
+
+for a in [3, 5, 7]:
+    # start = 2 - a%2 # b%2 must = a%2 to avoid all sequences monotonically increasing (and taking lots of time)
+    for b in [1, 3, 5]:
+        horizline = "-" * 20
+        print horizline
+        print "f(n)=%dn+%d or n/2" % (a,b)
+        print horizline
+        cf = newcollatzfunc(a, b, 2)
+        dolongreport(cf, 10000, maxint = 10**9)
+        print "\n"
